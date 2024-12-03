ca0315f523b7eae909a83ec660cd1c0fa6b0b05e
jakubgaba <j.gaba@email.cz>
2024-12-03 19:53:06 +0100

    Starting

--- src/main/java/com/xany/camel/App.java
+++ src/main/java/com/xany/camel/App.java
@@ -10,7 +10,7 @@ import org.apache.camel.impl.DefaultCamelContext;
  */
 public class App 
 {
-    public static void main( String[] args ) throws InterruptedException, Exception {
+    public static void main( String[] args ) throws Exception {
         CamelContext context = new DefaultCamelContext();
 
         context.addRoutes(new Router());
